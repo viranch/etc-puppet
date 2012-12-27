b6c5b2168b943b6692f09d34189d713446e84a65
Viranch Mehta <viranch.mehta@gmail.com>
2012-12-28 01:28:23 +0530

    Fix usage of user variable, again

--- manifests/templates.pp
+++ manifests/templates.pp
@@ -5,24 +5,25 @@ class base-node($user) {
   package { $packages: ensure => installed }
 
   $home = "/home/${user}"
+  $the_user = $user
 
   exec { 'clone-dotfiles':
     command => "/usr/bin/git clone git://github.com/viranch/dotfiles.git ${home}/.dotfiles",
-    user => "${base-node::user}",
+    user => $the_user,
     creates => "${home}/.dotfiles",
     require => User[$user],
   }
 
   exec { 'pull-dotfiles':
     command => '/usr/bin/git pull',
-    user => "${base-node::user}",
+    user => $the_user,
     cwd => "${home}/.dotfiles",
     require => Exec['clone-dotfiles'],
   }
 
   exec { 'run-setup-script':
     command => "${home}/.dotfiles/setup.sh",
-    user => "${base-node::user}",
+    user => $the_user,
     require => Exec['pull-dotfiles'],
   }
 
