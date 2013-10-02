52042fdbc55d7eb0d5d8897686d03cbd56a0fe31
Viranch Mehta <viranch.mehta@gmail.com>
2013-10-02 15:04:43 +0530

    Icinga web- install mod_cgi packages

--- modules/icinga/manifests/web.pp
+++ modules/icinga/manifests/web.pp
@@ -1,9 +1,12 @@
 class icinga::web {
   include apache
 
+  $mod_cgi = ['mod_fastcgi', 'mod_fcgid']
+  package { $mod_cgi: ensure => installed }
+
   file { '/etc/httpd/conf/extra/icinga.cfg':
     ensure => link,
     target => '/etc/icinga/apache.example.conf',
-    require => Package['icinga','apache'],
+    require => Package['icinga', 'apache', $mod_cgi],
   }
 }
