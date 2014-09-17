8ab0e83d06df0404eac3750790ddd0e938cde15f
Viranch Mehta <viranch.mehta@gmail.com>
2014-09-18 00:16:47 +0530

    Icinga- add icinga user & group

--- modules/icinga/manifests/init.pp
+++ modules/icinga/manifests/init.pp
@@ -1,4 +1,18 @@
 class icinga {
+  group { 'icinga':
+    ensure => present,
+    before => User['icinga'],
+  }
+
+  user { 'icinga':
+    ensure => present,
+    home   => '/dev/null',
+    shell  => '/bin/false',
+    gid    => 'icinga',
+    groups => 'http',
+    before => Package['icinga'],
+  }
+
   package { ['icinga', 'nagios-plugins']:
     ensure => installed,
   }
