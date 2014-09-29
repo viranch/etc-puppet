7e0970aafb40b67cd7a97733678e64a87fa66af3
Viranch Mehta <viranch.mehta@gmail.com>
2014-09-29 15:38:53 +0530

    Icinga conf- fix default file resource

--- modules/icinga/manifests/conf.pp
+++ modules/icinga/manifests/conf.pp
@@ -1,16 +1,16 @@
 class icinga::conf {
 
-  File {
-    ensure => directory,
-    recurse => true,
-    notify => Service['icinga'],
-  }
-
   file {
     '/etc/icinga/conf.d':
+      ensure => directory,
+      recurse => true,
+      notify => Service['icinga'],
       source => 'puppet:///modules/icinga/conf.d';
 
     '/etc/icinga':
+      ensure => directory,
+      recurse => true,
+      notify => Service['icinga'],
       source => 'puppet:///modules/icinga/conf';
   }
 
