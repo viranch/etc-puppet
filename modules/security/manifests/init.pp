5153b7889c32748a82388e1651edea5a6b41e9e2
Viranch Mehta <viranch.mehta@gmail.com>
2015-01-27 12:32:21 +0530

    security- fix cron definition

--- modules/security/manifests/init.pp
+++ modules/security/manifests/init.pp
@@ -1,8 +1,9 @@
 class security {
   # homebrew fail2ban
-  cron { '/opt/scripts/system/ban_ssh.sh':
-    user   => 'root',
-    hour   => 23,
-    minute => 59,
+  cron { 'ban-ssh':
+    command => '/opt/scripts/system/ban_ssh.sh',
+    user    => 'root',
+    hour    => 23,
+    minute  => 59,
   }
 }
